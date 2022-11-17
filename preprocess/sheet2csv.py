5bedb10fdf6124183f85c4fed9483f2c99e0e6c3
komatsu-taku <tak3k_1999@yahoo.co.jp>
2022-11-17 14:05:22 +0900

    [Add] csv2sheet of S-setN

--- preprocess/sheet2csv.py
+++ preprocess/sheet2csv.py
@@ -3,14 +3,14 @@ from cmath import nan
 import csv
 from turtle import pd
 
-from classopt import classopt
+from classopt import classopt, config
 import pandas as pd
 
 @classopt(default_long=True)
 class Args:
-    data_dir: str = "/home/initial/RFCM/RelationalFutureCaptioningModel/data/S-set4/sentences/"
-    csv_path: str = "S-set4_1001to1500.csv"
-    save_path: str = "sentences_1001to1500.csv"
+    data_dir: str = config(long="--dir", short='-d', default='/home/initial/RFCM/RelationalFutureCaptioningModel/data/BillaS/S-set4/sentences/')
+    csv_path: str = config(long="--csv", short="-c", default="S-set4_all.csv")
+    output_path: str = config(long="--output", short="-o", default="sentences_S-set4.csv")
 
 def main():
     args = Args.from_args()
@@ -18,10 +18,10 @@ def main():
     df = pd.read_csv(args.data_dir+args.csv_path)
     df = df[2:]
 
-    df_is_col = df[df["collision"] == 1]
-    df_is_col = df_is_col[["SCENE", "Ver1"]]
+    df_is_col = df[df["isColl"] == 1]
+    df_is_col = df_is_col[["Scene", "SentenceVer1"]]
 
-    df_is_col.to_csv(args.data_dir+args.save_path, header=None)
+    df_is_col.to_csv(args.data_dir+args.output_path, index=False)
 
 if __name__ == "__main__":
     main()
