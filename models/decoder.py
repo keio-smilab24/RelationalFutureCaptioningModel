bc219cf134baf8e07aacee6bcff6bf814a488c25
hiranohachiman <shinhirano@keio.jp>
2023-04-16 06:25:47 +0900

    [add] dropout each CA

--- models/decoder.py
+++ models/decoder.py
@@ -3,8 +3,8 @@ from torch import nn
 import torch.nn.functional as F
 
 from models.attentions import MultiHeadAttention
-from models.misc import FeedforwardNeuralNetModel, make_pad_shifted_mask, Intermediate
-from models.encoder import CrossAttentionEncoder
+from models.misc import FeedforwardNeuralNetModel, make_pad_shifted_mask
+
 
 class DecoderLayer(nn.Module):
     def __init__(self, cfg):
@@ -13,27 +13,35 @@ class DecoderLayer(nn.Module):
         self.LayerNorm = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
         self.LayerNorm2 = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
         self.LayerNorm3 = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
+        self.LayerNorm4 = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
+        self.LayerNorm5 = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
+        self.LayerNorm6 = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
+        self.LayerNorm7 = nn.LayerNorm(cfg.hidden_size, eps=cfg.layer_norm_eps)
 
-        # cross attention
+        # attention
         self.attention = MultiHeadAttention(cfg)
         self.attention2 = MultiHeadAttention(cfg)
         self.attention3 = MultiHeadAttention(cfg)
+        self.attention4 = MultiHeadAttention(cfg)
+        self.attention5 = MultiHeadAttention(cfg)
+        self.attention6 = MultiHeadAttention(cfg)
+        self.attention7 = MultiHeadAttention(cfg)
+
         self.rand = torch.randn(1, requires_grad=True).cuda()
         self.rand2 = torch.randn(1, requires_grad=True).cuda()
         self.rand3 = torch.randn(1, requires_grad=True).cuda()
+        self.dropout = nn.Dropout(cfg.hidden_dropout_prob)
+        self.dropout2 = nn.Dropout(cfg.hidden_dropout_prob)
+        self.dropout3 = nn.Dropout(cfg.hidden_dropout_prob)
+        self.dropout4 = nn.Dropout(cfg.hidden_dropout_prob)
+        self.dropout5 = nn.Dropout(cfg.hidden_dropout_prob)
+        self.dropout6 = nn.Dropout(cfg.hidden_dropout_prob)
+        self.dropout7 = nn.Dropout(cfg.hidden_dropout_prob)
+
 
         # ffn
         self.ffn = FeedforwardNeuralNetModel(cfg.hidden_size, cfg.hidden_size * 2, cfg.hidden_size)
         self.rand_z = torch.randn(1, requires_grad=True).cuda()
-        self.rand_z2 = torch.randn(1, requires_grad=True).cuda()
-        self.rand_z3 = torch.randn(1, requires_grad=True).cuda()
-
-        # self attention
-        self.selfmha = MultiHeadAttention(cfg)
-        self.linear2 = Intermediate(cfg)
-        self.rand4 = torch.randn(1, requires_grad=True).cuda()
-        # self.rand_z2 = torch.randn(1, requires_grad=True).cuda()
-
 
     def forward(
         self,
@@ -42,46 +50,81 @@ class DecoderLayer(nn.Module):
         clip_his: torch.Tensor,
         make_knn_dstore: bool=False
         ):
+        # attention layer
+        # first layer
+        q1 = x
+        kv1 = clip_his
 
-        """
-        # attention layer (default)
-        identity_x = x.clone().cuda()
-        att = self.attention(x=x, source_kv=clip_his)
-        x = self.rand*identity_x + (1 - self.rand)*att
-        x = self.LayerNorm(x)
-        """
-
-
+        x_text1 = self.attention(x=q1, source_kv = kv1)
+        x_text1 = self.dropout(x_text1)
+        x_text1 = x_text1 + q1
+        x_text1= self.LayerNorm(x_text1)
         
-        # attention layer (img : kv, text : q)
-        att = self.attention(x=x, source_kv=clip_his)
-        x_text = x + att
+        q2 = clip_his
+        kv2 = x
 
-        # attention layer (img : q, text : kv)
-        att = self.attention2(x=clip_his, source_kv=x)
-        x_img = clip_his + att
+        x_img1 = self.attention2(x=q2, source_kv=kv2)
+        x_img1 = self.dropout2(x_img1)
+        x_img1 = x_img1 + q2
+        x_img1 = self.LayerNorm2(x_img1)
 
-        # attention layer (x_1 : kv, x_2 : q)
-        att = self.attention3(x=x_text, source_kv=x_img)
-        x_text = x_text + att
-        x = self.LayerNorm(x_text)
+        # secoind layer
+        q3 = x_text1
+        kv3 = x_img1
 
+        x_text2 = self.attention3(x=q3, source_kv=kv3)
+        x_text2 = self.dropout3(x_text2)
+        x_text2 = x_text2 + q3
+        x_text2 = self.LayerNorm3(x_text2)
 
-        if make_knn_dstore: #最後の文字なら
-            knn_feat = x.clone().detach().cpu()
+        q4 = x_img1
+        kv4 = x_text1
+
+        x_img2 = self.attention4(x=q4, source_kv=kv4)
+        x_img2 = self.dropout4(x_img2)
+        x_img2 = x_img2 + q4
+        x_img2 = self.LayerNorm4(x_img2)
+        """
+        # tird layer 
+        q5 = x_text2
+        kv5 = x_img2
+
+        x_text3 = self.attention3(x=q5, source_kv=kv5)
+        x_text3 = self.dropout5(x_text3)
+        x_text3 = x_text3 + q5
+        x_text3 = self.LayerNorm5(x_text3)
+
+        q6 = x_img2
+        kv6 = x_text2
+
+        x_img3 = self.attention3(x=q6, source_kv=kv6)
+        x_img3 = self.dropout6(x_img3)
+        x_img3 = x_img3 + q6
+        x_img3 = self.LayerNorm6(x_img3)
+        """
+
+        # final attention layer
+        q7 = x_text2
+        kv7 = x_img2
+
+        x_final = self.attention3(x=q7, source_kv=kv7)
+        x_final = self.dropout7(x_final)
+        x_final = x_final + q7
+        x_final = self.LayerNorm7(x_final)
+
+        if make_knn_dstore: 
+            knn_feat = x_final.clone().detach().cpu()
 
         # ffn
-        identity_x = x.clone().cuda()
-        x = self.ffn(x)
-        output = self.rand_z*identity_x + (1 - self.rand_z)*x
+        identity_x = x_final.clone().cuda()
+        x_final = self.ffn(x_final)
+        output = self.rand_z*identity_x + (1 - self.rand_z)*x_final
         output = self.LayerNorm(output)
 
-        if make_knn_dstore: #最後の文字なら
+        if make_knn_dstore:
             return output, knn_feat
 
         return output
-
-
 
 
 class TransformerDecoder(nn.Module):
@@ -113,6 +156,7 @@ class TransformerDecoder(nn.Module):
             return all_layer_outputs, knn_feats
         else:
             return all_layer_outputs
+
 
 class TrmFeedForward(nn.Module):
     """
